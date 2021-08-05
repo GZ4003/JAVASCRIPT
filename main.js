23a5c0fed0d7039c98ba052c37323b4e28981f4a
GZ4003 <you@example.com>
2021-08-05 11:49:57 -0300

    nada gg

---
+++ main.js
@@ -0,0 +1,42 @@
+// control + corchetes  o llaves iugal a cometarios
+// Alt Gr +  corchetes === ``
+
+
+
+// const saludar = (name) => {
+//     return `Hola ${name}`
+// }
+
+// const  despedida = message => {
+//     let result;
+//     return result = message
+// }
+
+// const presentacion = (name, message) =>{
+//     let part1 = saludar (name)
+//     let part2 = despedida (message)
+
+//     return `${part1} ${part2}`
+// }
+
+// presentacion ('alexis', 'hasta luego, hasta pronto')
+
+const whileloop = () => {
+    let i = 1;
+
+    while( i < 11) {
+        console.log(i)
+        i++
+    }
+}
+
+const participantes = () => {
+    let e = 4;
+
+    while(e < 8) {
+        console.log(e)
+        e++
+    }
+}
+
+participantes();
